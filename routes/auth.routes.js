fbbb7efb5e8639036a040960f5111ace663f2e97
JuantoL <juanmarigonzalez2015@gmail.com>
2025-10-01 10:15:09 +0200

    Test 3

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { register, login, logout,autoLogin } = require("../controllers/auth.controller");
-
+const verifyToken = require("../middleware/authMiddleware");
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
